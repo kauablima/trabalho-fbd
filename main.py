6a2fe50a0c06c193a86749ada0d6346ae0dd3baf
kauablima <dev.kaualima@gmail.com>
2026-07-02 22:24:47 -0300

    refactor: atualizando a main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from campeonato_service import router as campeonato_router
+from crud_aposta import router as aposta_router
 
 app = FastAPI(
     title="API futbet",
     version="0.0.1"
 )
 
-app.include_router(campeonato_router, prefix="/campeonatos")
+app.include_router(aposta_router, prefix="/apostas")
